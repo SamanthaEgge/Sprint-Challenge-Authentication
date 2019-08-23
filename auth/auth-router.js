743ca151232368b44b525f1cc890aa5128792fa3
unknown <samantha.c.egge@gmail.com>
2019-08-23 08:38:34 -0700

    Registration, and fetching users works. Wrote up routes to login, haven't tested yet.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcyptjs')
+const bcrypt = require('bcryptjs')
 
 const Users = require('../users/users-model.js')
 
@@ -14,12 +14,41 @@ router.post('/register', (request, response) => {
       response.status(201).json(user)
     })
     .catch(error => {
-      response.status(500).response.json(error)
+      console.log(error)
+      response.status(500).response.json({ message: 'Issue with creating user on server' })
     })
 });
 
 router.post('/login', (request, response) => {
   // implement login
+  let { username, password } = request.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        response.status(200).json({ message: `Howdy, ${user.username}!`})
+      } else {
+        response.status(401).response.json({ message: 'These are incorrect credentials. Check your username and password, then try again' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).json(error);
+    })
 });
+
+
+//// for self testing only
+router.get('/users', (request, response) => {
+  Users.find()
+    .then(users => {
+      response.json(users)
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).response.json({ message: 'error retrieving users' })
+    })
+})
 
 module.exports = router;
